4906dcf66f7f07a713579dc2b999a95693743675
jens stein <jens.s.stein@gmail.com>
2013-07-12 11:04:15 +0200

    initial commit

---
+++ src/dk/jens/openbackup/AppInfo.java
@@ -0,0 +1,47 @@
+package dk.jens.openbackup;
+
+public class AppInfo
+{
+    String label, packageName, version, sourceDir, dataDir, lastBackup;
+    public boolean isSystem, isInstalled;
+
+    public AppInfo(String packageName, String label, String versionName, String sourceDir, String dataDir, String lastBackup, boolean isSystem, boolean isInstalled)
+    {
+        this.label = label;
+        this.packageName = packageName;
+        this.version = versionName;
+        this.sourceDir = sourceDir;
+        this.dataDir = dataDir;
+        this.lastBackup = lastBackup;
+        this.isSystem = isSystem;
+        this.isInstalled = isInstalled;
+    }
+    public String getPackageName()
+    {
+        return packageName;
+    }
+    public String getLabel()
+    {
+        return label;
+    }
+    public String getVersion()
+    {
+        return version;
+    }
+    public String getSourceDir()
+    {
+        return sourceDir;
+    }
+    public String getDataDir()
+    {
+        return dataDir;
+    }
+    public String getLastBackupTimestamp()
+    {
+        return lastBackup;
+    }
+    public String toString()
+    {
+        return label + " : " + packageName + " : " + lastBackup;
+    }
+}
