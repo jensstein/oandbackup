cb4d9407bf788919abc7a9a6b6b7911de7a48dce
jens stein <jens.s.stein@gmail.com>
2017-04-14 11:16:51 +0200

    Use Notification instead of NotificationCompat

--- src/dk/jens/backup/ui/NotificationHelper.java
+++ src/dk/jens/backup/ui/NotificationHelper.java
@@ -1,11 +1,11 @@
 package dk.jens.backup.ui;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import dk.jens.backup.R;
 
@@ -16,7 +16,7 @@ public class NotificationHelper
         if(!PreferenceManager.getDefaultSharedPreferences(context).getBoolean("disableNotifications", false))
         {
     //        Notification.Builder mBuilder = new Notification.Builder(this)
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+            Notification.Builder mBuilder = new Notification.Builder(context)
                 .setSmallIcon(R.drawable.backup2_small)
                 .setContentTitle(title)
                 .setContentText(text)
