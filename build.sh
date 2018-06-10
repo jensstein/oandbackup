57451218992b027166553e8aa96502f6af332573
jens stein <jens.s.stein@gmail.com>
2018-06-10 17:25:11 +0200

    Add build script for both android and rust code
    This build script doesn't build the rust code for other architectures
    than armv7-linux-androideabi. Other architectures will be added.

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+function build {
+	cargo_options=""
+	gradle_mode=""
+	if test $1 = "release"; then
+		cargo_options="--release"
+		gradle_mode="build"
+	elif test $1 = "debug"; then
+		cargo_options=""
+		gradle_mode="assembleDebug"
+	fi
+
+	cd $(realpath $(dirname $0))
+
+	cd oab-utils
+	set -x
+	cargo test
+	cargo build $cargo_options --target armv7-linux-androideabi
+	cp -v target/armv7-linux-androideabi/release/oab-utils ../assets
+
+	cd ../
+	./gradlew $gradle_mode
+	set +x
+}
+
+if test $# -eq 0; then
+	printf "usage: $0 {debug|release}\n"
+	exit 1
+fi
+
+while test $# -gt 0; do
+	case $1 in
+	"release" | "debug")
+		build $1
+		;;
+	*)
+		printf "unknown option $1\n"
+		exit 1
+		;;
+	esac
+	shift
+done
