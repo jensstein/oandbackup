7e7ddc2475d11b072e170608404542152eb74189
jens stein <jens.s.stein@gmail.com>
2019-01-06 11:46:43 +0100

    Use database for schedule data in boot receiver

--- src/main/java/dk/jens/backup/schedules/BootReceiver.java
+++ src/main/java/dk/jens/backup/schedules/BootReceiver.java
@@ -4,42 +4,53 @@ import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import dk.jens.backup.Constants;
-import dk.jens.backup.OAndBackup;
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
+import dk.jens.backup.schedules.db.Schedule;
+import dk.jens.backup.schedules.db.ScheduleDao;
+import dk.jens.backup.schedules.db.ScheduleDatabase;
+import dk.jens.backup.schedules.db.ScheduleDatabaseHelper;
+
+import java.util.List;
 
 public class BootReceiver extends BroadcastReceiver
 {
-    static final String TAG = OAndBackup.TAG;
-    
-    SharedPreferences prefs;
-    HandleAlarms handleAlarms;
-
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        handleAlarms = new HandleAlarms(context);
-        prefs = context.getSharedPreferences(Constants.PREFS_SCHEDULES, 0);
-        for(int i = 0; i <= prefs.getInt(Constants.PREFS_SCHEDULES_TOTAL, 0); i++)
-        {
-            if(prefs.getBoolean(Constants.PREFS_SCHEDULES_ENABLED + i, false) &&
-                prefs.getInt(Constants.PREFS_SCHEDULES_REPEATTIME + i, 0) > 0)
-            {
-                long timePlaced = prefs.getLong(Constants.PREFS_SCHEDULES_TIMEPLACED + i, 0);
-                long repeat = (long)(prefs.getInt(
-                    Constants.PREFS_SCHEDULES_REPEATTIME + i, 0) * AlarmManager.INTERVAL_DAY);
-                long timePassed = System.currentTimeMillis() - timePlaced;
-                long timeLeft = prefs.getLong(
-                    Constants.PREFS_SCHEDULES_TIMEUNTILNEXTEVENT + i, 0) - timePassed;
-                if(timeLeft < (5 * 60000))
-                {
-                    handleAlarms.setAlarm(i, AlarmManager.INTERVAL_FIFTEEN_MINUTES, repeat);
-                }
-                else
-                {
-                    handleAlarms.setAlarm(i, timeLeft, repeat);
-                }
+        final HandleAlarms handleAlarms = getHandleAlarms(context);
+        final ScheduleDao scheduleDao = getScheduleDao(context,
+            Scheduler.DATABASE_NAME);
+        final List<Schedule> schedules = Stream.of(scheduleDao.getAll())
+            .filter(schedule -> schedule.isEnabled() &&
+            schedule.getInterval() > 0).collect(Collectors.toList());
+        for(Schedule schedule : schedules) {
+            final long repeat = schedule.getInterval() *
+                AlarmManager.INTERVAL_DAY;
+            final long timePassed = getCurrentTime() - schedule
+                .getPlaced();
+            final long timeLeft = schedule.getTimeUntilNextEvent() -
+                timePassed;
+            if(timeLeft < (5 * 60000)) {
+                handleAlarms.setAlarm((int)schedule.getId(),
+                    AlarmManager.INTERVAL_FIFTEEN_MINUTES, repeat);
+            } else {
+                handleAlarms.setAlarm((int)schedule.getId(), timeLeft, repeat);
             }
         }
+    }
+
+    long getCurrentTime() {
+        return System.currentTimeMillis();
+    }
+
+    HandleAlarms getHandleAlarms(Context context) {
+        return new HandleAlarms(context);
+    }
+
+    ScheduleDao getScheduleDao(Context context, String databasename) {
+        final ScheduleDatabase scheduleDatabase = ScheduleDatabaseHelper
+            .getScheduleDatabase(context, databasename);
+        return scheduleDatabase.scheduleDao();
     }
 }
