1e2b62e4e6a665fb795955793c2caf721dcd7619
jens stein <jens.s.stein@gmail.com>
2019-01-15 19:44:33 +0100

    Remove option to disable notifications
    The system itself should handle disabling notifications if the users
    wants it.

--- src/main/java/dk/jens/backup/ui/NotificationHelper.java
+++ src/main/java/dk/jens/backup/ui/NotificationHelper.java
@@ -5,36 +5,32 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.preference.PreferenceManager;
 import dk.jens.backup.R;
 
 public class NotificationHelper
 {
     public static void showNotification(Context context, Class c, int id, String title, String text, boolean autocancel)
     {
-        if(!PreferenceManager.getDefaultSharedPreferences(context).getBoolean("disableNotifications", false))
-        {
     //        Notification.Builder mBuilder = new Notification.Builder(this)
-            Notification.Builder mBuilder = new Notification.Builder(context)
-                .setSmallIcon(R.drawable.backup2_small)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setAutoCancel(autocancel);
-            Intent resultIntent = new Intent(context, c);
-            resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                // make sure messages aren't overdrawn - taskstackbuilder doesn't seem to work well with single_top
-            /*
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-            stackBuilder.addParentStack(c);
-            stackBuilder.addNextIntent(resultIntent);
-            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-            */
-            PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification.Builder mBuilder = new Notification.Builder(context)
+            .setSmallIcon(R.drawable.backup2_small)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setAutoCancel(autocancel);
+        Intent resultIntent = new Intent(context, c);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            // make sure messages aren't overdrawn - taskstackbuilder doesn't seem to work well with single_top
+        /*
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(c);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        */
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            mBuilder.setContentIntent(resultPendingIntent);
+        mBuilder.setContentIntent(resultPendingIntent);
 
-            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            mNotificationManager.notify(id, mBuilder.build());
-        }
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(id, mBuilder.build());
     }
 }
