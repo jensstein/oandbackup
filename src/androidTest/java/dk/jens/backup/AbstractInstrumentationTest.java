131e9cb5e3573672037bfd0726c91ab9dbefe715
jens stein <jens.s.stein@gmail.com>
2019-01-15 21:51:52 +0100

    Use notification channel on new android versions
    As of sdk version 26 (android 8) you must create a notification channel
    before trying to show a notification.
    Creating identical notification channels multiple times should be fine
    because the system should be able to you the existing one.
    https://developer.android.com/training/notify-user/build-notification

--- src/androidTest/java/dk/jens/backup/AbstractInstrumentationTest.java
+++ src/androidTest/java/dk/jens/backup/AbstractInstrumentationTest.java
@@ -16,7 +16,7 @@ public abstract class AbstractInstrumentationTest {
         "android.permission.READ_EXTERNAL_STORAGE",
         "android.permission.WRITE_EXTERNAL_STORAGE");
 
-    void grantRootPrivileges() {
+    protected void grantRootPrivileges() {
         // https://developer.android.com/training/testing/ui-automator
         final UiDevice device = UiDevice.getInstance(
             InstrumentationRegistry.getInstrumentation());
--- src/main/java/dk/jens/backup/ui/NotificationHelper.java
+++ src/main/java/dk/jens/backup/ui/NotificationHelper.java
@@ -1,36 +1,57 @@
 package dk.jens.backup.ui;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import dk.jens.backup.Constants;
 import dk.jens.backup.R;
 
-public class NotificationHelper
-{
-    public static void showNotification(Context context, Class c, int id, String title, String text, boolean autocancel)
-    {
-    //        Notification.Builder mBuilder = new Notification.Builder(this)
-        Notification.Builder mBuilder = new Notification.Builder(context)
+public class NotificationHelper {
+    private static final String TAG = Constants.TAG;
+
+    public static void showNotification(Context context, Class parentActivity,
+            int id, String title, String text, boolean autocancel) {
+
+        final Intent resultIntent = new Intent(context, parentActivity);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent resultPendingIntent = PendingIntent.getActivity(
+            context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final String channelId = Constants.TAG;
+        if(Build.VERSION.SDK_INT >= 26) {
+            final NotificationChannel notificationChannel =
+                new NotificationChannel(channelId, channelId,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            final NotificationManager notificationManager = context
+                .getSystemService(NotificationManager.class);
+            if(notificationManager != null) {
+                notificationManager.createNotificationChannel(
+                    notificationChannel);
+            } else {
+                Log.w(TAG, String.format(
+                    "Unable to create notification channel %s", channelId));
+            }
+        }
+
+        final Notification notification = new NotificationCompat.Builder(
+                context, channelId)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setSmallIcon(R.drawable.backup2_small)
             .setContentTitle(title)
             .setContentText(text)
-            .setAutoCancel(autocancel);
-        Intent resultIntent = new Intent(context, c);
-        resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            // make sure messages aren't overdrawn - taskstackbuilder doesn't seem to work well with single_top
-        /*
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addParentStack(c);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        */
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            .setAutoCancel(autocancel)
+            .setContentIntent(resultPendingIntent)
+            .build();
 
-        mBuilder.setContentIntent(resultPendingIntent);
-
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(id, mBuilder.build());
+        final NotificationManagerCompat notificationManager =
+            NotificationManagerCompat.from(context);
+        notificationManager.notify(id, notification);
     }
 }
