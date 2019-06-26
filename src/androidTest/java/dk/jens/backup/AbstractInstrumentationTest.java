f18a4183e0bc04e9ada12a809c86edb1dbe75146
jens stein <jens.s.stein@gmail.com>
2019-06-26 22:04:40 +0200

    Allow more flexibility for getting root privileges
    This should allow different root granting dialogs.

--- src/androidTest/java/dk/jens/backup/AbstractInstrumentationTest.java
+++ src/androidTest/java/dk/jens/backup/AbstractInstrumentationTest.java
@@ -7,6 +7,7 @@ import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
+import com.annimon.stream.Optional;
 import org.junit.Rule;
 
 public abstract class AbstractInstrumentationTest {
@@ -21,13 +22,31 @@ public abstract class AbstractInstrumentationTest {
             InstrumentationRegistry.getInstrumentation());
         final UiObject rememberCheckBox = device.findObject(new UiSelector()
             .checkable(true));
-        final UiObject confirmButton = device.findObject(new UiSelector()
-            .text("ALLOW"));
-        try {
-            rememberCheckBox.click();
-            confirmButton.click();
-        } catch (UiObjectNotFoundException e) {
-            Log.w(Constants.TAG, "Unable to grant root privileges from test");
+        final Optional<UiObject> confirmButton = getConfirmButton(device);
+        if(!confirmButton.isPresent()) {
+            Log.w(Constants.TAG,
+                "Unable to find root granting confirmation button");
+        } else {
+            try {
+                if(rememberCheckBox.exists()) {
+                    rememberCheckBox.click();
+                }
+                confirmButton.get().click();
+            } catch (UiObjectNotFoundException e) {
+                Log.w(Constants.TAG, "Unable to grant root privileges from test");
+            }
         }
+    }
+
+    private Optional<UiObject> getConfirmButton(UiDevice device) {
+        final String[] possibleTexts = new String[] {"ALLOW", "GRANT"};
+        for(String s : possibleTexts) {
+            final UiObject confirmButton = device.findObject(new UiSelector()
+                .text(s));
+            if(confirmButton.exists()) {
+                return Optional.of(confirmButton);
+            }
+        }
+        return Optional.empty();
     }
 }
