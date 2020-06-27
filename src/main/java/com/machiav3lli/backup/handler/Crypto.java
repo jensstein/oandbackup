76e8a46ffe8b67359479413d1e471a827acc2d9c
tiefkuehlpizze <piz2920@gmail.com>
2020-06-28 00:18:39 +0200

    Fixed: Crypto class to be a valid Android implementation

--- src/main/java/com/machiav3lli/backup/handler/Crypto.java
+++ src/main/java/com/machiav3lli/backup/handler/Crypto.java
@@ -17,33 +17,51 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * Crypto. The class to handle encryption and decryption of streams.
+ * Call `encryptStream` or `decryptStream` with a password and a salt or a better a secret key
+ * (for performance reasons) and the class will wrap the given stream in return.
+ * <p>
+ * Android Keystore API is not used on purpose, because the key material needs to be portable for
+ * uses cases when the device has been wiped or when backups are restored on another device.
+ * <p>
+ * The IV is static as it may be public.
+ */
 public final class Crypto {
     public static final String TAG = Constants.classTag(".Crypto");
 
     /**
-     * Taken from here. Chosen because of SDK 24+ compatibility
+     * Taken from here. Chosen because of API Level 24+ compatibility. Newer algorithms are available
+     * with API Level 26+.
      * https://developer.android.com/guide/topics/security/cryptography#SupportedSecretKeyFactory
+     * <p>
+     * The actual choice was inspired by this blog post:
+     * https://www.raywenderlich.com/778533-encryption-tutorial-for-android-getting-started
      */
-    public static final String DEFAULT_SECRET_KEY_FACTORY_ALGORITHM = "HmacSHA256";
-    public static final String DEFAULT_CIPHER_ALGORITHM = "AES_128/CBC/NoPadding";
-    public static final int DEFAULT_IV_BLOCK_SIZE = 16;
+    public static final String DEFAULT_SECRET_KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA1";
+    public static final String DEFAULT_CIPHER_ALGORITHM = "AES_128/CBC/PKCS5Padding";
+    public static final int DEFAULT_IV_BLOCK_SIZE = 16;  // 128 bit
+    public static final int ITERATION_COUNT = 1000;
+    public static final int KEY_LENGTH = 128;
 
-    public static SecretKey generateKeyFromPassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return Crypto.generateKeyFromPassword(password, Crypto.DEFAULT_SECRET_KEY_FACTORY_ALGORITHM, Crypto.DEFAULT_CIPHER_ALGORITHM);
+    public static SecretKey generateKeyFromPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return Crypto.generateKeyFromPassword(password, salt, Crypto.DEFAULT_SECRET_KEY_FACTORY_ALGORITHM, Crypto.DEFAULT_CIPHER_ALGORITHM);
     }
 
-    public static SecretKey generateKeyFromPassword(String password, String keyFactoryAlgorithm, String cipherAlgorithm) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public static SecretKey generateKeyFromPassword(String password, byte[] salt, String keyFactoryAlgorithm, String cipherAlgorithm) throws NoSuchAlgorithmException, InvalidKeySpecException {
         SecretKeyFactory factory = SecretKeyFactory.getInstance(keyFactoryAlgorithm);
-        KeySpec spec = new javax.crypto.spec.PBEKeySpec(password.toCharArray());
-        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), cipherAlgorithm.split("/")[0]);
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, Crypto.ITERATION_COUNT, Crypto.KEY_LENGTH);
+        byte[] keyBytes = factory.generateSecret(spec).getEncoded();
+        SecretKey secret = new SecretKeySpec(keyBytes, cipherAlgorithm.split("/")[0]);
         return secret;
     }
 
-    public static CipherOutputStream encryptStream(OutputStream os, String password) throws CryptoSetupException {
+    public static CipherOutputStream encryptStream(OutputStream os, String password, byte[] salt) throws CryptoSetupException {
         try {
-            SecretKey secret = Crypto.generateKeyFromPassword(password);
+            SecretKey secret = Crypto.generateKeyFromPassword(password, salt);
             return Crypto.encryptStream(os, secret);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             Log.e(Crypto.TAG, "Could not setup encryption: " + e.getMessage());
