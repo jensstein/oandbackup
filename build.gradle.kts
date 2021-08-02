17dfe9bd95b1009476bf5ad4158d9c8eba0b8094
machiav3lli <antonios.hazim@tutanota.com>
2021-08-03 01:15:43 +0200

    Update: Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 }
