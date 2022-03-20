204368dcff43aa91a6b4570dc4c01ddd60df4d20
machiav3lli <antonios.hazim@tutanota.com>
2022-03-20 21:21:11 +0100

    Add: Horizontal item list compose component

--- app/src/main/java/com/machiav3lli/backup/ui/compose/recycler/Components.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/recycler/Components.kt
@@ -1,12 +1,10 @@
 package com.machiav3lli.backup.ui.compose.recycler
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -44,6 +42,40 @@ fun <T> VerticalItemList(
                 LazyColumn(
                     verticalArrangement = Arrangement.Absolute.spacedBy(8.dp),
                     contentPadding = PaddingValues(vertical = 8.dp, horizontal = 8.dp),
+                ) {
+                    items(items = list, key = itemKey, itemContent = itemContent)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun <T> HorizontalItemList(
+    modifier: Modifier = Modifier,
+    list: List<T>?,
+    itemKey: ((T) -> Any)? = null,
+    itemContent: @Composable LazyItemScope.(T) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .fillMaxWidth(),
+        contentAlignment = if (list.isNullOrEmpty()) Alignment.Center else Alignment.CenterStart
+    ) {
+        when {
+            list == null -> Text(
+                text = stringResource(id = R.string.loading_list),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            list.isEmpty() -> Text(
+                text = stringResource(id = R.string.empty_filtered_list),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            else -> {
+                LazyRow(
+                    horizontalArrangement = Arrangement.Absolute.spacedBy(4.dp),
+                    contentPadding = PaddingValues(vertical = 4.dp, horizontal = 4.dp),
                 ) {
                     items(items = list, key = itemKey, itemContent = itemContent)
                 }
