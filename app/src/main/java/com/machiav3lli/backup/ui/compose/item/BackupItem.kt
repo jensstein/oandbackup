2a1de62ee99eb70861ebd4b8043c57581d04cb43
Harald Gutsche <hg42@gmx.net>
2022-03-29 14:04:56 +0200

    show backupVersion

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/BackupItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/BackupItem.kt
@@ -2,9 +2,18 @@ package com.machiav3lli.backup.ui.compose.item
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -80,9 +89,18 @@ fun BackupItem(
                     style = MaterialTheme.typography.bodySmall,
                     color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
-                AnimatedVisibility(visible = item.isEncrypted) {
+                Text(
+                    text = if(item.backupVersionCode == 0) "old" else "${item.backupVersionCode/1000}.${item.backupVersionCode%1000}",
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+                AnimatedVisibility(visible = item.isCompressed) {
                     Text(
-                        text = item.cipherType ?: "",
+                        text = " - ${item.compressionType}",
                         modifier = Modifier.align(Alignment.CenterVertically),
                         softWrap = true,
                         overflow = TextOverflow.Ellipsis,
@@ -91,9 +109,9 @@ fun BackupItem(
                         color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 }
-                AnimatedVisibility(visible = item.isCompressed) {
+                AnimatedVisibility(visible = item.isEncrypted) {
                     Text(
-                        text = " - ${item.compressionType}",
+                        text = " - ${item.cipherType}",
                         modifier = Modifier.align(Alignment.CenterVertically),
                         softWrap = true,
                         overflow = TextOverflow.Ellipsis,
