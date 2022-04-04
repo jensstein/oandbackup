cd2377292f535820eec2a6b8352b1d3d8cb0fb4c
Harald Gutsche <hg42@gmx.net>
2022-04-04 19:03:56 +0200

    add backup count to list items

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/MainPackageItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/MainPackageItem.kt
@@ -2,9 +2,19 @@ package com.machiav3lli.backup.ui.compose.item
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -86,9 +96,9 @@ fun MainPackageItem(
                     )
                     AnimatedVisibility(visible = item.hasBackups) {
                         Text(
-                            text = item.latestBackup?.backupDate?.getFormattedDate(
+                            text = (item.latestBackup?.backupDate?.getFormattedDate(
                                 false
-                            ) ?: "",
+                                    ) ?: "") + " - ${item.backupHistory.size}",
                             modifier = Modifier.align(Alignment.CenterVertically),
                             overflow = TextOverflow.Ellipsis,
                             maxLines = 1,
