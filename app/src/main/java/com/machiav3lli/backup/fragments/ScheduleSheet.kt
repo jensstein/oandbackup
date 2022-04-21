8bfea5f4a91846f0d7c7788e97238ce90c194bac
machiav3lli <antonios.hazim@tutanota.com>
2022-04-21 02:05:39 +0200

    Add: ScheduleSheet's compose content

--- app/src/main/java/com/machiav3lli/backup/fragments/ScheduleSheet.kt
+++ app/src/main/java/com/machiav3lli/backup/fragments/ScheduleSheet.kt
@@ -26,7 +26,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.children
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import com.machiav3lli.backup.MAIN_FILTER_DEFAULT
 import com.machiav3lli.backup.MODE_UNSET
@@ -39,8 +67,27 @@ import com.machiav3lli.backup.dialogs.IntervalInDaysDialog
 import com.machiav3lli.backup.dialogs.PackagesListDialogFragment
 import com.machiav3lli.backup.dialogs.ScheduleNameDialog
 import com.machiav3lli.backup.handler.ShellCommands
+import com.machiav3lli.backup.mainFilterChipItems
+import com.machiav3lli.backup.schedSpecialFilterChipItems
+import com.machiav3lli.backup.scheduleBackupModeChipItems
 import com.machiav3lli.backup.services.ScheduleService
-import com.machiav3lli.backup.utils.*
+import com.machiav3lli.backup.ui.compose.item.ActionChip
+import com.machiav3lli.backup.ui.compose.item.CheckChip
+import com.machiav3lli.backup.ui.compose.item.ElevatedActionButton
+import com.machiav3lli.backup.ui.compose.item.RoundButton
+import com.machiav3lli.backup.ui.compose.item.TitleText
+import com.machiav3lli.backup.ui.compose.recycler.MultiSelectableChipGroup
+import com.machiav3lli.backup.ui.compose.recycler.SelectableChipGroup
+import com.machiav3lli.backup.ui.compose.theme.AppTheme
+import com.machiav3lli.backup.ui.item.ChipItem
+import com.machiav3lli.backup.utils.calculateTimeToRun
+import com.machiav3lli.backup.utils.cancelAlarm
+import com.machiav3lli.backup.utils.filterToString
+import com.machiav3lli.backup.utils.modeToModes
+import com.machiav3lli.backup.utils.modesToString
+import com.machiav3lli.backup.utils.scheduleAlarm
+import com.machiav3lli.backup.utils.specialBackupsEnabled
+import com.machiav3lli.backup.utils.specialFilterToString
 import com.machiav3lli.backup.viewmodels.ScheduleViewModel
 import java.lang.ref.WeakReference
 import java.time.LocalTime
@@ -66,6 +113,7 @@ class ScheduleSheet(private val scheduleId: Long) : BaseSheet() {
         viewModel = ViewModelProvider(this, viewModelFactory)[ScheduleViewModel::class.java]
 
         viewModel.schedule.observe(viewLifecycleOwner) {
+            redrawPage()
         }
 
         return binding.root
@@ -247,5 +295,178 @@ class ScheduleSheet(private val scheduleId: Long) : BaseSheet() {
             viewModel.schedule.value?.blockList = newList
             refresh(false)
         }.show(requireActivity().supportFragmentManager, "BLOCKLIST_DIALOG")
+    }
+
+    @OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
+    fun redrawPage() {
+        binding.composeView.setContent {
+            AppTheme(
+                darkTheme = isSystemInDarkTheme()
+            ) {
+                viewModel.schedule.value?.let {
+                    Column(
+                        modifier = Modifier.height(IntrinsicSize.Min)
+                    ) {
+                        CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onBackground) {
+                            Column(
+                                modifier = Modifier
+                                    .background(color = Color.Transparent)
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 8.dp)
+                                    .weight(1f)
+                                    .verticalScroll(rememberScrollState()),
+                                verticalArrangement = Arrangement.spacedBy(4.dp)
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                                ) {
+                                    TitleText(R.string.sched_name)
+                                    Text(
+                                        text = it.name,
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .clickable { showNameEditorDialog() },
+                                        style = MaterialTheme.typography.titleLarge,
+                                        textAlign = TextAlign.Center,
+                                    )
+                                    RoundButton(
+                                        icon = painterResource(id = R.drawable.ic_arrow_down),
+                                        description = stringResource(id = R.string.dismiss),
+                                        onClick = { dismissAllowingStateLoss() }
+                                    )
+                                }
+                                Row(
+                                    modifier = Modifier.height(IntrinsicSize.Min),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                                ) {
+                                    TitleText(R.string.sched_hourOfDay)
+                                    Text(
+                                        text = LocalTime.of(it.timeHour, it.timeMinute).toString(),
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .fillMaxHeight()
+                                            .clickable { showTimePickerDialog() },
+                                        color = MaterialTheme.colorScheme.primary,
+                                        textAlign = TextAlign.End,
+                                    )
+                                }
+                                Row(
+                                    modifier = Modifier.height(IntrinsicSize.Min),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                                ) {
+                                    TitleText(R.string.sched_interval)
+                                    Text(
+                                        text = it.interval.toString(),
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .fillMaxHeight()
+                                            .clickable { showIntervalSetterDialog() },
+                                        color = MaterialTheme.colorScheme.primary,
+                                        textAlign = TextAlign.End,
+                                    )
+                                }
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                                ) {
+                                    ActionChip(
+                                        icon = painterResource(id = R.drawable.ic_customlist),
+                                        text = stringResource(id = R.string.customListTitle),
+                                        positive = it.customList.isNotEmpty(),
+                                        fullWidth = true,
+                                        modifier = Modifier.weight(1f),
+                                        onClick = { showCustomListDialog() }
+                                    )
+                                    ActionChip(
+                                        icon = painterResource(id = R.drawable.ic_blocklist),
+                                        text = stringResource(id = R.string.sched_blocklist),
+                                        positive = it.blockList.isNotEmpty(),
+                                        fullWidth = true,
+                                        modifier = Modifier.weight(1f),
+                                        onClick = { showBlockListDialog() }
+                                    )
+                                }
+                                TitleText(R.string.filter_options)
+                                MultiSelectableChipGroup(
+                                    list = if (requireContext().specialBackupsEnabled) mainFilterChipItems
+                                    else mainFilterChipItems.minus(ChipItem.Special),
+                                    selectedFlags = it.filter
+                                ) { flag ->
+                                    it.filter = it.filter xor flag
+                                    refresh(false)
+                                }
+                                TitleText(R.string.sched_mode)
+                                MultiSelectableChipGroup(
+                                    list = scheduleBackupModeChipItems,
+                                    selectedFlags = it.mode
+                                ) { flag ->
+                                    it.mode = it.mode xor flag
+                                    refresh(false)
+                                }
+                                TitleText(R.string.other_filters_options)
+                                SelectableChipGroup(
+                                    list = schedSpecialFilterChipItems,
+                                    selectedFlag = it.specialFilter
+                                ) { flag ->
+                                    it.specialFilter = flag
+                                    refresh(false)
+                                }
+                            }
+
+                        }
+                        CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurface) {
+                            Column(
+                                modifier = Modifier
+                                    .background(color = MaterialTheme.colorScheme.surface)
+                                    .fillMaxWidth()
+                                    .padding(8.dp)
+                                    .wrapContentHeight(),
+                                verticalArrangement = Arrangement.spacedBy(4.dp)
+                            ) {
+                                Row() {
+                                    AnimatedVisibility(visible = it.enabled) {
+                                        Text(
+                                            text = "${stringResource(id = R.string.sched_timeLeft)} "
+                                                .plus(getTimeLeft(it))
+                                        )
+                                    }
+                                }
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    CheckChip(
+                                        checked = it.enabled,
+                                        textId = R.string.sched_checkbox,
+                                        checkedTextId = R.string.enabled,
+                                        onCheckedChange = { checked ->
+                                            it.enabled = checked
+                                            refresh(true)
+                                        }
+                                    )
+                                    Spacer(modifier = Modifier.weight(1f))
+                                    ActionChip(
+                                        text = stringResource(id = R.string.delete),
+                                        icon = painterResource(id = R.drawable.ic_delete),
+                                        positive = false,
+                                        withText = false
+                                    ) {
+                                        viewModel.deleteSchedule()
+                                        cancelAlarm(requireContext(), scheduleId)
+                                        dismissAllowingStateLoss()
+                                    }
+                                }
+                                ElevatedActionButton(
+                                    text = stringResource(id = R.string.sched_activateButton),
+                                    icon = painterResource(id = R.drawable.ic_backup),
+                                    fullWidth = true,
+                                    onClick = { startSchedule() }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
