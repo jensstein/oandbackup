9684331d1dc290ddf13e3f6ad924016250b63737
Harald Gutsche <hg42@gmx.net>
2022-04-29 22:55:46 +0200

    add version name to HelpSheet

--- app/src/main/java/com/machiav3lli/backup/fragments/HelpSheet.kt
+++ app/src/main/java/com/machiav3lli/backup/fragments/HelpSheet.kt
@@ -49,8 +49,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.HtmlCompat
+import com.machiav3lli.backup.BuildConfig
 import com.machiav3lli.backup.R
 import com.machiav3lli.backup.legendList
 import com.machiav3lli.backup.linksList
@@ -77,6 +79,12 @@ class HelpSheet : BaseSheet() {
         }
     }
 
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        HelpPage()
+    }
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun HelpPage() {
@@ -96,6 +104,12 @@ class HelpSheet : BaseSheet() {
                             style = MaterialTheme.typography.headlineMedium,
                             modifier = Modifier.weight(1f),
                             maxLines = 1
+                        )
+                        Text(
+                            text = BuildConfig.VERSION_NAME, //.replace("-", " "),
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.weight(1f),
+                            maxLines = 3
                         )
                         RoundButton(icon = painterResource(id = R.drawable.ic_arrow_down)) {
                             dismissAllowingStateLoss()
