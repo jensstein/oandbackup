01a26e3a6f9020b335f896f34802bcce6986322b
machiav3lli <antonios.hazim@tutanota.com>
2022-05-03 02:24:51 +0200

    Update: Version name style in HelpSheet

--- app/src/main/java/com/machiav3lli/backup/fragments/HelpSheet.kt
+++ app/src/main/java/com/machiav3lli/backup/fragments/HelpSheet.kt
@@ -29,9 +29,11 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
@@ -49,6 +51,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.HtmlCompat
@@ -96,20 +99,24 @@ class HelpSheet : BaseSheet() {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
+                            .height(IntrinsicSize.Min)
                             .padding(horizontal = 8.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Text(
                             text = stringResource(id = R.string.app_name),
                             style = MaterialTheme.typography.headlineMedium,
-                            modifier = Modifier.weight(1f),
                             maxLines = 1
                         )
                         Text(
-                            text = BuildConfig.VERSION_NAME, //.replace("-", " "),
-                            style = MaterialTheme.typography.bodySmall,
-                            modifier = Modifier.weight(1f),
-                            maxLines = 3
+                            text = BuildConfig.VERSION_NAME,
+                            style = MaterialTheme.typography.titleLarge,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier
+                                .padding(start = 8.dp)
+                                .weight(1f),
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
                         )
                         RoundButton(icon = painterResource(id = R.drawable.ic_arrow_down)) {
                             dismissAllowingStateLoss()
