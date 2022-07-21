07cac97e6f1af7d4253ce8f1b027ad55fd696c37
machiav3lli <antonios.hazim@tutanota.com>
2022-07-21 03:21:15 +0200

    Add: Compose BaseDialog & EnumDialogUI

---
+++ app/src/main/java/com/machiav3lli/backup/dialogs/ComposeDialog.kt
@@ -0,0 +1,107 @@
+package com.machiav3lli.backup.dialogs
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.machiav3lli.backup.OABX
+import com.machiav3lli.backup.R
+import com.machiav3lli.backup.ui.compose.item.ActionButton
+import com.machiav3lli.backup.ui.compose.item.ElevatedActionButton
+import com.machiav3lli.backup.ui.compose.item.SelectableRow
+import com.machiav3lli.backup.ui.item.Pref
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BaseDialog(
+    openDialogCustom: MutableState<Boolean>,
+    dialogUI: @Composable (() -> Unit)
+) {
+    Dialog(
+        onDismissRequest = { openDialogCustom.value = false },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        dialogUI()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EnumDialogUI(
+    pref: Pref.EnumPref,
+    openDialogCustom: MutableState<Boolean>
+) {
+    val context = LocalContext.current
+    var selected by remember { mutableStateOf(OABX.prefInt(pref.key, pref.defaultValue)) }
+    val entryPairs = pref.entries.toList()
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(text = stringResource(pref.titleId), style = MaterialTheme.typography.titleLarge)
+            LazyColumn(
+                modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
+            ) {
+                items(items = entryPairs) {
+                    val isSelected = rememberSaveable(selected) {
+                        mutableStateOf(selected == it.first)
+                    }
+                    SelectableRow(
+                        modifier = Modifier.clip(MaterialTheme.shapes.medium),
+                        title = stringResource(id = it.second),
+                        selectedState = isSelected
+                    ) {
+                        selected = it.first
+                    }
+                }
+            }
+
+            Row(
+                Modifier.fillMaxWidth()
+            ) {
+                ActionButton(text = stringResource(id = R.string.dialogCancel)) {
+                    openDialogCustom.value = false
+                }
+                Spacer(Modifier.weight(1f))
+                ElevatedActionButton(text = stringResource(id = R.string.dialogSave)) {
+                    OABX.setPrefInt(pref.key, selected)
+                    openDialogCustom.value = false
+                }
+            }
+        }
+    }
+}
