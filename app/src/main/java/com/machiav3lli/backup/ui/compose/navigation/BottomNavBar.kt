98f0202eea21e75afbcbe4b3fa501a47ff7d3aec
machiav3lli <antonios.hazim@tutanota.com>
2022-07-22 01:28:04 +0200

    Update: BottomNavBar (items) layout

--- app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/BottomNavBar.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/BottomNavBar.kt
@@ -1,18 +1,16 @@
 package com.machiav3lli.backup.ui.compose.navigation
 
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.machiav3lli.backup.NAV_MAIN
@@ -37,7 +35,6 @@ fun BottomNavBar(page: Int = NAV_MAIN, navController: NavController) {
     }
 
     NavigationBar(
-        modifier = Modifier.height(72.dp),
         containerColor = MaterialTheme.colorScheme.surface,
         contentColor = MaterialTheme.colorScheme.onSurface
     ) {
@@ -56,11 +53,15 @@ fun BottomNavBar(page: Int = NAV_MAIN, navController: NavController) {
                 label = {
                     Text(
                         text = stringResource(id = item.title),
-                        style = MaterialTheme.typography.bodySmall,
+                        style = MaterialTheme.typography.bodyMedium,
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
                     )
                 },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = MaterialTheme.colorScheme.primary,
+                    selectedIconColor = MaterialTheme.colorScheme.onPrimary
+                ),
                 alwaysShowLabel = false,
                 selected = selected,
                 onClick = {
