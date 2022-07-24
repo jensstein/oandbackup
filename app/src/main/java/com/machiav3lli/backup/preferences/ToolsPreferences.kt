628908dbfb502c24f25ade00042e1d2bfbacf406
machiav3lli <antonios.hazim@tutanota.com>
2022-07-25 01:57:06 +0200

    Add: SnackbarHost to Tools prefs page

--- app/src/main/java/com/machiav3lli/backup/preferences/ToolsPreferences.kt
+++ app/src/main/java/com/machiav3lli/backup/preferences/ToolsPreferences.kt
@@ -6,7 +6,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -23,9 +28,12 @@ import com.machiav3lli.backup.ui.compose.theme.Exodus
 import com.machiav3lli.backup.ui.compose.theme.ExtDATA
 import com.machiav3lli.backup.ui.item.Pref
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToolsPrefsPage() {
     val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
     val prefs = listOf(
         CleanupBackupFolderPref,
         CopySelfPref,
@@ -37,14 +45,15 @@ fun ToolsPrefsPage() {
     AppTheme(
         darkTheme = isSystemInDarkTheme()
     ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(items = prefs) {
-                LaunchPreference(pref = it) {
-                    // TODO add special action to each when (it) { }
+        Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(items = prefs) { pref ->
+                    LaunchPreference(pref = pref) {
+                    }
                 }
             }
         }
