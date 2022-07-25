d0668884de8b972e5ee6b24938f227856370b901
machiav3lli <antonios.hazim@tutanota.com>
2022-07-25 02:05:00 +0200

    Add: Logic for uninstalled apps batch cleaner

--- app/src/main/java/com/machiav3lli/backup/preferences/ToolsPreferences.kt
+++ app/src/main/java/com/machiav3lli/backup/preferences/ToolsPreferences.kt
@@ -1,5 +1,8 @@
 package com.machiav3lli.backup.preferences
 
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,24 +12,33 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.machiav3lli.backup.OABX
 import com.machiav3lli.backup.PREFS_BATCH_DELETE
 import com.machiav3lli.backup.PREFS_COPYSELF
 import com.machiav3lli.backup.PREFS_LOGVIEWER
 import com.machiav3lli.backup.PREFS_SAVEAPPSLIST
 import com.machiav3lli.backup.PREFS_SCHEDULESEXPORTIMPORT
 import com.machiav3lli.backup.R
+import com.machiav3lli.backup.activities.PrefsActivityX
+import com.machiav3lli.backup.handler.showNotification
+import com.machiav3lli.backup.items.Package
 import com.machiav3lli.backup.ui.compose.item.LaunchPreference
 import com.machiav3lli.backup.ui.compose.theme.AppTheme
 import com.machiav3lli.backup.ui.compose.theme.DeData
 import com.machiav3lli.backup.ui.compose.theme.Exodus
 import com.machiav3lli.backup.ui.compose.theme.ExtDATA
 import com.machiav3lli.backup.ui.item.Pref
+import com.machiav3lli.backup.utils.FileUtils.invalidateBackupLocation
+import com.machiav3lli.backup.utils.show
+import kotlinx.coroutines.CoroutineScope
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -53,6 +65,12 @@ fun ToolsPrefsPage() {
             ) {
                 items(items = prefs) { pref ->
                     LaunchPreference(pref = pref) {
+                        when (pref) {
+                            CleanupBackupFolderPref -> context.onClickUninstalledBackupsDelete(
+                                snackbarHostState,
+                                coroutineScope
+                            )
+                        }
                     }
                 }
             }
@@ -67,6 +85,71 @@ val CleanupBackupFolderPref = Pref.LinkPref(
     iconId = R.drawable.ic_delete,
     //iconTint = MaterialTheme.colorScheme.secondary
 )
+
+private fun Context.onClickUninstalledBackupsDelete(
+    snackbarHostState: SnackbarHostState,
+    coroutineScope: CoroutineScope
+): Boolean {
+    val deleteList = ArrayList<Package>()
+    val message = StringBuilder()
+    val packageList = OABX.main?.viewModel?.packageList?.value ?: emptyList()
+    if (packageList.isNotEmpty()) {
+        packageList.forEach { appInfo ->
+            if (!appInfo.isInstalled) {
+                deleteList.add(appInfo)
+                message.append(appInfo.packageLabel).append("\n")
+            }
+        }
+    }
+    if (packageList.isNotEmpty()) {
+        if (deleteList.isNotEmpty()) {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.prefs_batchdelete)
+                .setMessage(message.toString().trim { it <= ' ' })
+                .setPositiveButton(R.string.dialogYes) { _: DialogInterface?, _: Int ->
+                    deleteBackups(deleteList)
+                    invalidateBackupLocation()
+                }
+                .setNegativeButton(R.string.dialogNo, null)
+                .show()
+        } else {
+            snackbarHostState.show(
+                coroutineScope,
+                getString(R.string.batchDeleteNothingToDelete)
+            )
+        }
+    } else {
+        snackbarHostState.show(
+            coroutineScope,
+            getString(R.string.wait_noappslist),
+        )
+    }
+    return true
+}
+
+private fun Context.deleteBackups(deleteList: List<Package>) {
+    val notificationId = System.currentTimeMillis().toInt()
+    deleteList.forEachIndexed { i, ai ->
+        showNotification(
+            this,
+            PrefsActivityX::class.java,
+            notificationId,
+            "${getString(R.string.batchDeleteMessage)} ($i/${deleteList.size})",
+            ai.packageLabel,
+            false
+        )
+        Timber.i("deleting backups of ${ai.packageLabel}")
+        ai.deleteAllBackups()
+    }
+    showNotification(
+        this,
+        PrefsActivityX::class.java,
+        notificationId,
+        getString(R.string.batchDeleteNotificationTitle),
+        "${getString(R.string.batchDeleteBackupsDeleted)} ${deleteList.size}",
+        false
+    )
+}
 
 val CopySelfPref = Pref.LinkPref(
     key = PREFS_COPYSELF,
