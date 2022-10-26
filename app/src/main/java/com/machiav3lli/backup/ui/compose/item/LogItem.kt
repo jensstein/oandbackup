2d4bdcef828398e080207fd54664c97e54347744
Harald Gutsche <hg42@gmx.net>
2022-10-26 22:47:11 +0200

    improve LogItem, maxheight to prevent endless scrolling, should also be faster in display

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/LogItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/LogItem.kt
@@ -1,16 +1,21 @@
 package com.machiav3lli.backup.ui.compose.item
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
@@ -19,14 +24,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.machiav3lli.backup.R
 import com.machiav3lli.backup.items.Log
 import com.machiav3lli.backup.ui.compose.icons.Phosphor
-import com.machiav3lli.backup.ui.compose.icons.phosphor.CaretDown
-import com.machiav3lli.backup.ui.compose.icons.phosphor.CaretUp
 import com.machiav3lli.backup.ui.compose.icons.phosphor.ShareNetwork
 import com.machiav3lli.backup.ui.compose.icons.phosphor.TrashSimple
 import com.machiav3lli.backup.ui.compose.theme.LocalShapes
@@ -38,9 +42,7 @@ fun LogItem(
     onShare: (Log) -> Unit = {},
     onDelete: (Log) -> Unit = {}
 ) {
-    val expanded = remember { mutableStateOf(false) }
     OutlinedCard(
-        modifier = Modifier.clickable { expanded.value = ! expanded.value },
         shape = RoundedCornerShape(LocalShapes.current.medium),
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.surface),
         colors = CardDefaults.outlinedCardColors(
@@ -63,79 +65,87 @@ fun LogItem(
                         .wrapContentHeight()
                         .weight(1f)
                 ) {
-                    Row(
-                        modifier = Modifier.wrapContentHeight(),
-                    ) {
-                        Text(
-                            text = item.logDate.getFormattedDate(true) ?: "",
-                            modifier = Modifier
-                                .align(Alignment.CenterVertically)
-                                .weight(1f),
-                            softWrap = true,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = MaterialTheme.typography.titleMedium
+                    Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+                        Column(modifier = Modifier.weight(1f, true)) {
+                            Text(
+                                text = item.logDate.getFormattedDate(true) ?: "",
+                                softWrap = true,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                            Row {
+                                if (!item.deviceName.isNullOrEmpty())
+                                    Text(
+                                        text = "${item.deviceName} ",
+                                        overflow = TextOverflow.Ellipsis,
+                                        maxLines = 1,
+                                        style = MaterialTheme.typography.bodySmall,
+                                    )
+                                if (!item.sdkCodename.isNullOrEmpty())
+                                    Text(
+                                        text = "abi${item.sdkCodename} ",
+                                        softWrap = true,
+                                        overflow = TextOverflow.Ellipsis,
+                                        maxLines = 1,
+                                        style = MaterialTheme.typography.bodySmall
+                                    )
+                                if (!item.cpuArch.isNullOrEmpty())
+                                    Text(
+                                        text = "${item.cpuArch} ",
+                                        overflow = TextOverflow.Ellipsis,
+                                        maxLines = 1,
+                                        style = MaterialTheme.typography.bodySmall,
+                                    )
+                            }
+                        }
+                        ElevatedActionButton(
+                            icon = Phosphor.ShareNetwork,
+                            text = stringResource(id = R.string.shareTitle),
+                            withText = false,
+                            positive = true,
+                            onClick = { onShare(item) }
                         )
-                        Text(
-                            text = item.deviceName ?: "",
-                            modifier = Modifier.align(Alignment.CenterVertically),
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = MaterialTheme.typography.bodySmall,
-                        )
-                    }
-                    Row(
-                        modifier = Modifier.wrapContentHeight(),
-                    ) {
-                        Text(
-                            text = item.sdkCodename ?: "",
-                            modifier = Modifier
-                                .align(Alignment.CenterVertically)
-                                .weight(1f),
-                            softWrap = true,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        Text(
-                            text = item.cpuArch ?: "",
-                            modifier = Modifier.align(Alignment.CenterVertically),
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = MaterialTheme.typography.bodySmall,
+                        ElevatedActionButton(
+                            icon = Phosphor.TrashSimple,
+                            text = stringResource(id = R.string.delete),
+                            withText = false,
+                            positive = false,
+                            onClick = { onDelete(item) }
                         )
                     }
                 }
-                ElevatedActionButton(
-                    icon = Phosphor.ShareNetwork,
-                    text = stringResource(id = R.string.shareTitle),
-                    withText = false,
-                    positive = true,
-                    onClick = { onShare(item) }
-                )
-                ElevatedActionButton(
-                    icon = Phosphor.TrashSimple,
-                    text = stringResource(id = R.string.delete),
-                    withText = false,
-                    positive = false,
-                    onClick = { onDelete(item) }
-                )
             }
-            var text = item.logText ?: ""
-            if ( ! expanded.value )
-                text = text.substringBefore("\n\n===========").lines().subList(0, 10).joinToString("\n")
-            Text(
-                text = text,
-                style = MaterialTheme.typography.bodySmall
-            )
-            Icon(
-                modifier = Modifier.fillMaxWidth(),
-                imageVector = if (expanded.value)
-                    Phosphor.CaretUp
-                else
-                    Phosphor.CaretDown,
-                contentDescription = null
-            )
+
+            val lines = remember { mutableStateOf(item.logText?.lines() ?: listOf()) }
+            SelectionContainer {
+                Card {
+                    val maxLines = 20
+                    val typo = MaterialTheme.typography.bodySmall
+                    if (lines.value.size < maxLines) {
+                        Column {
+                            lines.value.forEach {
+                                Text(text = it, style = typo)
+                            }
+                        }
+                    } else {
+                        val listState = rememberLazyListState()
+                        Box(modifier = Modifier
+                            .fillMaxWidth()
+                            .height(with(LocalDensity.current) { typo.lineHeight.toDp() }*maxLines)
+                        ) {
+                            LazyColumn(modifier = Modifier
+                                .fillMaxWidth(),
+                                state = listState
+                            ) {
+                                items(lines.value) {
+                                    Text(text = it, style = typo)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
