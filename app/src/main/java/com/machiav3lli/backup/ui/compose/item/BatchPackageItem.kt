5790eb079c9c9aecaaebd1b445e71b30ec591df8
machiav3lli <antonios.hazim@tutanota.com>
2022-11-14 04:27:43 +0100

    Update: Simplify Batch PackageItem

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/BatchPackageItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/BatchPackageItem.kt
@@ -1,20 +1,18 @@
 package com.machiav3lli.backup.ui.compose.item
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,13 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.machiav3lli.backup.OABX
 import com.machiav3lli.backup.items.Package
 import com.machiav3lli.backup.ui.compose.theme.LocalShapes
 import com.machiav3lli.backup.utils.getFormattedDate
-import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -63,14 +60,12 @@ fun BatchPackageItem(
     }
     //Timber.i("recompose BatchPackageItem ${packageItem.packageName}")
 
-    OutlinedCard(
+    Card(
         modifier = Modifier,
         shape = RoundedCornerShape(LocalShapes.current.medium),
-        border = BorderStroke(1.dp, MaterialTheme.colorScheme.surface),
-        colors = CardDefaults.outlinedCardColors(
-            containerColor = MaterialTheme.colorScheme.background
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
         ),
-        elevation = CardDefaults.cardElevation(4.dp),
         onClick = {
             val checked = (apkChecked || !showApk) && (dataChecked || !showData)
             if (showApk) apkChecked = !checked
@@ -103,11 +98,7 @@ fun BatchPackageItem(
             Column(
                 modifier = Modifier.wrapContentHeight()
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.4f),
-                ) {
+                Row(modifier = Modifier.fillMaxWidth()) {
                     Text(
                         text = packageItem.packageLabel,
                         modifier = Modifier
@@ -120,11 +111,7 @@ fun BatchPackageItem(
                     )
                     PackageLabels(item = packageItem)
                 }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.4f),
-                ) {
+                Row(modifier = Modifier.fillMaxWidth()) {
                     Text(
                         text = packageItem.packageName,
                         modifier = Modifier
