746f67bcfe9d3f8d638102bfed3b6042300eada7
Harald Gutsche <hg42@gmx.net>
2022-11-24 21:20:54 +0100

    LogsPage: fix logText not updated when deleting items (remove remember!)

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/LogItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/LogItem.kt
@@ -13,8 +13,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -109,7 +107,7 @@ fun LogItem(
                 }
             }
 
-            val lines = remember { mutableStateOf(item.logText?.lines() ?: listOf()) }
+            val lines = item.logText?.lines() ?: listOf()
             Card(modifier = Modifier.fillMaxWidth()) {
                 val maxLines = 20
                 val lineHeight = with(LocalDensity.current) { 10.sp.toDp() }
@@ -119,7 +117,7 @@ fun LogItem(
                         .padding(0.dp)
                         .background(color = Color(0.2f, 0.2f, 0.3f))
                 ) {
-                    TerminalText(lines.value, scrollOnAdd = false)
+                    TerminalText(lines, scrollOnAdd = false)
                 }
             }
         }
