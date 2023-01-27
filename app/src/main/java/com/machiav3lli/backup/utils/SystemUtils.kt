dc9b2e088f3f7a056a465e90fe1e2a47934a367a
Harald Gutsche <hg42@gmx.net>
2023-01-27 19:03:55 +0100

    add runParallel()

--- app/src/main/java/com/machiav3lli/backup/utils/SystemUtils.kt
+++ app/src/main/java/com/machiav3lli/backup/utils/SystemUtils.kt
@@ -3,6 +3,17 @@ package com.machiav3lli.backup.utils
 import android.content.Context
 import android.content.pm.PackageManager
 import com.machiav3lli.backup.BuildConfig
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.ByteArrayInputStream
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
@@ -36,4 +47,37 @@ object SystemUtils {
     }
 
     val numCores = Runtime.getRuntime().availableProcessors()
+
+    suspend fun <T> runParallel(
+        items: List<T>,
+        scope: CoroutineScope = MainScope(),
+        pool: CoroutineDispatcher = Dispatchers.IO,
+        todo: (item: T) -> Unit
+    ) {
+        val list = items.toList()
+        when (1) {
+
+            // best,  8 threads, may hang with recursion
+            0 -> list.stream().parallel().forEach { todo(it) }
+
+            // slow,  7 threads with IO, most used once, one used 900 times
+            0 -> runBlocking { list.asFlow().onEach { todo(it) }.flowOn(pool).collect {} }
+
+            // slow,  1 thread with IO
+            0 -> list.asFlow().onEach { todo(it) }.collect {}
+
+            // slow, 19 threads with IO
+            0 -> list.asFlow().map { scope.launch(pool) { todo(it) } }.collect { it.join() }
+
+            // best, 63 threads with IO
+            0 -> runBlocking { list.asFlow().collect { launch(pool) { todo(it) } } }
+
+            // best, 66 threads with IO
+            0 -> list.map { scope.launch(pool) { todo(it) } }.joinAll()
+
+            // best, 63 threads with IO
+            1 -> runBlocking { list.forEach { launch(pool) { todo(it) } } }
+        }
+    }
+
 }
