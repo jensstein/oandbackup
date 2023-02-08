f188e08bf8d30d51b5c05e2e15924ad0806f6bfd
machiav3lli <antonios.hazim@tutanota.com>
2023-02-08 02:25:15 +0100

    Update: Revamp NavBar items layout

--- app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/BottomNavBar.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/BottomNavBar.kt
@@ -1,29 +1,14 @@
 package com.machiav3lli.backup.ui.compose.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.machiav3lli.backup.NAV_MAIN
 import com.machiav3lli.backup.NAV_PREFS
-import com.machiav3lli.backup.preferences.pref_squeezeNavText
-import com.machiav3lli.backup.ui.compose.item.ResponsiveText
 
 @Composable
 fun BottomNavBar(page: Int = NAV_MAIN, navController: NavController) {
@@ -53,46 +38,10 @@ fun BottomNavBar(page: Int = NAV_MAIN, navController: NavController) {
         items.forEach { item ->
             val selected = currentDestination == item.destination
 
-            NavigationBarItem(
-
-                icon = {
-                    Icon(
-                        imageVector = item.icon,
-                        contentDescription = stringResource(id = item.title),
-                        modifier = Modifier
-                            .background(
-                                if (selected) MaterialTheme.colorScheme
-                                    .surfaceColorAtElevation(48.dp)
-                                else Color.Transparent,
-                                CircleShape
-                            )
-                            .padding(8.dp)
-                            .size(if (selected) 36.dp else 26.dp),
-                    )
-                },
-                label = {
-                    if (!selected && pref_squeezeNavText.value)
-                        ResponsiveText(
-                            text = stringResource(id = item.title),
-                            maxLines = 1,
-                            textStyle = MaterialTheme.typography.bodyMedium,
-                        )
-                    else if (!selected)
-                        Text(
-                            text = stringResource(id = item.title),
-                            style = MaterialTheme.typography.bodyMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    indicatorColor = MaterialTheme.colorScheme.background,
-                    selectedIconColor = MaterialTheme.colorScheme.primary,
-                    unselectedIconColor = MaterialTheme.colorScheme.onSurface,
-                    unselectedTextColor = MaterialTheme.colorScheme.onSurface,
-                ),
-                alwaysShowLabel = true,
+            NavBarItem(
                 selected = selected,
+                icon = item.icon,
+                labelId = item.title,
                 onClick = {
                     navController.navigate(item.destination) {
                         navController.currentDestination?.id?.let {
--- app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/Pager.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/Pager.kt
@@ -1,20 +1,26 @@
 package com.machiav3lli.backup.ui.compose.navigation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -54,50 +60,64 @@ fun PagerNavBar(pageItems: List<NavItem>, pagerState: PagerState) {
         pageItems.forEachIndexed { index, tab ->
             val selected = pagerState.currentPage == index
 
-            NavigationBarItem(
+            NavBarItem(
                 selected = selected,
-                icon = {
-                    Icon(
-                        imageVector = tab.icon,
-                        contentDescription = stringResource(id = tab.title),
-                        modifier = Modifier
-                            .background(
-                                if (selected) MaterialTheme.colorScheme
-                                    .surfaceColorAtElevation(48.dp)
-                                else Color.Transparent,
-                                CircleShape
-                            )
-                            .padding(8.dp)
-                            .size(if (selected) 36.dp else 26.dp),
-                    )
-                },
-                label = {
-                    if (!selected && pref_squeezeNavText.value)
-                        ResponsiveText(
-                            text = stringResource(id = tab.title),
-                            maxLines = 1,
-                            textStyle = MaterialTheme.typography.bodyMedium,
-                        )
-                    else if (!selected)
-                        Text(
-                            text = stringResource(id = tab.title),
-                            style = MaterialTheme.typography.bodyMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                },
-                alwaysShowLabel = true,
-                colors = NavigationBarItemDefaults.colors(
-                    indicatorColor = MaterialTheme.colorScheme.background,
-                    selectedIconColor = MaterialTheme.colorScheme.primary,
-                    unselectedIconColor = MaterialTheme.colorScheme.onSurface,
-                    unselectedTextColor = MaterialTheme.colorScheme.onSurface,
-                ),
+                icon = tab.icon,
+                labelId = tab.title,
                 onClick = {
                     scope.launch { pagerState.scrollToPage(index) }
                 },
             )
         }
     }
+}
 
+@Composable
+fun RowScope.NavBarItem(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    labelId: Int,
+    selected: Boolean,
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = Modifier
+            .clickable { onClick() }
+            .weight(1f),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = stringResource(id = labelId),
+                modifier = Modifier
+                    .background(
+                        if (selected) MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp)
+                        else Color.Transparent,
+                        CircleShape
+                    )
+                    .padding(8.dp)
+                    .size(if (selected) 36.dp else 26.dp),
+                tint = if (selected) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.onSurface
+            )
+            if (!selected && pref_squeezeNavText.value)
+                ResponsiveText(
+                    text = stringResource(id = labelId),
+                    maxLines = 1,
+                    textStyle = MaterialTheme.typography.bodyMedium,
+                )
+            else if (!selected)
+                Text(
+                    text = stringResource(id = labelId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+        }
+    }
 }
