a952f341b82cb4bfac5a7f276436bf53ec47c854
Harald Gutsche <hg42@gmx.net>
2023-02-10 01:09:25 +0100

    cleanup

--- app/src/main/java/com/machiav3lli/backup/tasks/AppActionWork.kt
+++ app/src/main/java/com/machiav3lli/backup/tasks/AppActionWork.kt
@@ -122,7 +122,6 @@ class AppActionWork(val context: Context, workerParams: WorkerParameters) :
 
                     packageItem?.let { pi ->
                         try {
-                            //pi.refreshBackupList()  // not yet set, be up to date when the job is finally executed
                             OABX.shellHandlerInstance?.let { shellHandler ->
                                 actionResult = when {
                                     backupBoolean -> {
@@ -142,7 +141,6 @@ class AppActionWork(val context: Context, workerParams: WorkerParameters) :
                                     }
                                 }
                             }
-                            //pi.refreshBackupList()  // who knows what happened in external space?
                         } catch (e: Throwable) {
                             val message = "package not processed: $packageName $packageLabel: $e\n${
                                 LogsHandler.message(e, true)
