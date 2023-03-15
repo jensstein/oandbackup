368c979fa03eb1399085795ffb8124e51de4f1f5
Harald Gutsche <hg42@gmx.net>
2023-03-15 22:05:10 +0100

    make Test_selinux work again

--- app/src/androidTest/kotlin/tests/Test_selinux.kt
+++ app/src/androidTest/kotlin/tests/Test_selinux.kt
@@ -1,7 +1,9 @@
 package tests
 import androidx.test.platform.app.InstrumentationRegistry
-import com.machiav3lli.backup.handler.ShellHandler
-import org.junit.Assert.*
+import com.machiav3lli.backup.OABX
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import timber.log.Timber
 
@@ -10,7 +12,7 @@ class Test_selinux {
     @Test
     fun test_suGetOwnerGroupContext_extracts_valid_context() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val userGroupContext = ShellHandler().suGetOwnerGroupContext("/system")
+        val userGroupContext = OABX.shellHandlerInstance!!.suGetOwnerGroupContext("/system")
         val con = userGroupContext[2]
         Timber.i("suGetOwnerGroupContext -> ${userGroupContext.joinToString("', '", "'", "'")} => context = '$con'")
 
