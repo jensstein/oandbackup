a4c44dcf72c3e92a8ed108fa9489c57c77e8074b
Harald Gutsche <hg42@users.noreply.github.com>
2023-03-23 22:56:04 +0100

    set main icon according to variant

--- app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/Item.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/navigation/Item.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import com.machiav3lli.backup.OABX
+import com.machiav3lli.backup.OABX.Companion.isDebug
+import com.machiav3lli.backup.OABX.Companion.isHg42
+import com.machiav3lli.backup.OABX.Companion.isNeo
 import com.machiav3lli.backup.R
 import com.machiav3lli.backup.pages.BatchPage
 import com.machiav3lli.backup.pages.HomePage
@@ -17,9 +20,11 @@ import com.machiav3lli.backup.ui.compose.icons.phosphor.ArchiveTray
 import com.machiav3lli.backup.ui.compose.icons.phosphor.Bug
 import com.machiav3lli.backup.ui.compose.icons.phosphor.CalendarX
 import com.machiav3lli.backup.ui.compose.icons.phosphor.ClockCounterClockwise
+import com.machiav3lli.backup.ui.compose.icons.phosphor.Detective
 import com.machiav3lli.backup.ui.compose.icons.phosphor.Flask
 import com.machiav3lli.backup.ui.compose.icons.phosphor.GearSix
 import com.machiav3lli.backup.ui.compose.icons.phosphor.House
+import com.machiav3lli.backup.ui.compose.icons.phosphor.Infinity
 import com.machiav3lli.backup.ui.compose.icons.phosphor.SlidersHorizontal
 import com.machiav3lli.backup.ui.compose.icons.phosphor.UserGear
 import com.machiav3lli.backup.ui.compose.icons.phosphor.Warning
@@ -42,7 +47,16 @@ sealed class NavItem(var title: Int, var icon: ImageVector, var destination: Str
         NavItem(R.string.permission_not_granted, Phosphor.Warning, "intro_permissions")
 
     object Home :
-        NavItem(R.string.home, Phosphor.House, "home")
+        NavItem(
+            R.string.home,
+            when {
+                isNeo   -> Phosphor.Infinity
+                isDebug -> Phosphor.Bug
+                isHg42  -> Phosphor.Detective
+                else    -> Phosphor.House
+            },
+            "home"
+        )
 
     object Backup :
         NavItem(R.string.backup, Phosphor.ArchiveTray, "batch_backup")
@@ -89,7 +103,7 @@ sealed class NavItem(var title: Int, var icon: ImageVector, var destination: Str
     @Composable
     fun ComposablePage(navController: NavHostController) {
         when (destination) {
-            Home.destination -> HomePage()
+            Home.destination                        -> HomePage()
             Backup.destination, Restore.destination -> {
                 OABX.main?.let {
                     if (destination == Backup.destination) it.backupViewModel
@@ -98,15 +112,15 @@ sealed class NavItem(var title: Int, var icon: ImageVector, var destination: Str
                     BatchPage(viewModel = it, backupBoolean = destination == Backup.destination)
                 }
             }
-            Scheduler.destination -> {
+            Scheduler.destination                   -> {
                 OABX.main?.schedulerViewModel?.let { viewModel ->
                     SchedulerPage(viewModel)
                 }
             }
-            UserPrefs.destination -> UserPrefsPage()
-            ServicePrefs.destination -> ServicePrefsPage()
-            AdvancedPrefs.destination -> AdvancedPrefsPage()
-            ToolsPrefs.destination -> ToolsPrefsPage(navController = navController)
+            UserPrefs.destination                   -> UserPrefsPage()
+            ServicePrefs.destination                -> ServicePrefsPage()
+            AdvancedPrefs.destination               -> AdvancedPrefsPage()
+            ToolsPrefs.destination                  -> ToolsPrefsPage(navController = navController)
         }
     }
 }
