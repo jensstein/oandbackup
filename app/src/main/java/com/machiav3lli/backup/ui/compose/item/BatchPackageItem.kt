7c445c5b7a2d5d5f72da9a968d27a0e63ec53a35
machiav3lli <antonios.hazim@tutanota.com>
2023-03-23 11:08:29 +0100

    Add: RestorePackageItem composable

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/BatchPackageItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/BatchPackageItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,7 +25,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.machiav3lli.backup.R
+import com.machiav3lli.backup.dbs.entity.Backup
 import com.machiav3lli.backup.items.Package
+import com.machiav3lli.backup.ui.compose.icons.Phosphor
+import com.machiav3lli.backup.ui.compose.icons.phosphor.AsteriskSimple
+import com.machiav3lli.backup.ui.compose.icons.phosphor.CircleWavyWarning
+import com.machiav3lli.backup.ui.compose.icons.phosphor.Spinner
+import com.machiav3lli.backup.ui.compose.icons.phosphor.User
+import com.machiav3lli.backup.ui.compose.theme.ColorDisabled
+import com.machiav3lli.backup.ui.compose.theme.ColorSpecial
+import com.machiav3lli.backup.ui.compose.theme.ColorSystem
+import com.machiav3lli.backup.ui.compose.theme.ColorUpdated
+import com.machiav3lli.backup.ui.compose.theme.ColorUser
 import com.machiav3lli.backup.ui.compose.theme.LocalShapes
 import com.machiav3lli.backup.utils.getFormattedDate
 
@@ -134,6 +147,125 @@ fun BatchPackageItem(
                         )
                     }
                 }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RestorePackageItem(
+    item: Package,
+    apkBackupChecked: MutableState<Int?>,
+    dataBackupChecked: MutableState<Int?>,
+    onClick: (Package, Boolean, Boolean) -> Unit = { _: Package, _: Boolean, _: Boolean -> },
+    onBackupApkClick: (Backup, Boolean, Int) -> Unit = { _: Backup, _: Boolean, _: Int -> },
+    onBackupDataClick: (Backup, Boolean, Int) -> Unit = { _: Backup, _: Boolean, _: Int -> },
+) {
+    val packageItem by remember(item) { mutableStateOf(item) }
+    val apkBC by apkBackupChecked
+    val dataBC by dataBackupChecked
+    var apkChecked by remember(apkBackupChecked) { mutableStateOf(apkBC == 0) }
+    var dataChecked by remember(dataBackupChecked) { mutableStateOf(dataBC == 0) }
+    val checkableApk by remember(packageItem) {
+        mutableStateOf(packageItem.latestBackup?.hasApk == true)
+    }
+    val checkableData by remember(packageItem) {
+        mutableStateOf(packageItem.latestBackup?.hasData == true)
+    }
+
+    Card(
+        modifier = Modifier,
+        shape = RoundedCornerShape(LocalShapes.current.medium),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
+        ),
+        onClick = {
+            val checked = (apkChecked || !checkableApk) && (dataChecked || !checkableData)
+            if (checkableApk) apkChecked = !checked
+            if (checkableData) dataChecked = !checked
+            onClick(packageItem, apkChecked, dataChecked)
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.wrapContentHeight()
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = packageItem.packageLabel,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .weight(1f),
+                            softWrap = true,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        if (item.isUpdated) {
+                            ButtonIcon(
+                                Phosphor.CircleWavyWarning, R.string.radio_updated,
+                                tint = ColorUpdated
+                            )
+                        }
+                        ButtonIcon(
+                            when {
+                                item.isSpecial -> Phosphor.AsteriskSimple
+                                item.isSystem  -> Phosphor.Spinner
+                                else           -> Phosphor.User
+                            },
+                            R.string.app_s_type_title,
+                            tint = when {
+                                !item.isInstalled -> ColorDisabled
+                                item.isDisabled   -> ColorDisabled
+                                item.isSpecial    -> ColorSpecial
+                                item.isSystem     -> ColorSystem
+                                else              -> ColorUser
+                            }
+                        )
+                    }
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = packageItem.packageName,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .weight(1f),
+                            softWrap = true,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        AnimatedVisibility(visible = packageItem.hasBackups) {
+                            Text(
+                                text = (packageItem.latestBackup?.backupDate?.getFormattedDate(
+                                    false
+                                ) ?: "") + " • ${packageItem.numberOfBackups}",
+                                modifier = Modifier.align(Alignment.CenterVertically),
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                style = MaterialTheme.typography.bodySmall,
+                            )
+                        }
+                    }
+                }
+            }
+            item.backupsNewestFirst.forEachIndexed { index, item ->
+                RestoreBackupItem(
+                    item = item,
+                    index = index,
+                    isApkChecked = index == apkBC,
+                    isDataChecked = index == dataBC,
+                    onApkClick = onBackupApkClick,
+                    onDataClick = onBackupDataClick,
+                )
             }
         }
     }
