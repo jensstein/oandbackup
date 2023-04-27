1974712a5698cb13efdff549669f3540e801d83c
machiav3lli <antonios.hazim@tutanota.com>
2023-04-28 01:33:23 +0200

    Add: TimePicker DialogUI

---
+++ app/src/main/java/com/machiav3lli/backup/dialogs/TimePickerDialogUI.kt
@@ -0,0 +1,84 @@
+/*
+ * Neo Backup: open-source apps backup and restore app.
+ * Copyright (C) 2020  Antonios Hazim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.machiav3lli.backup.dialogs
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.machiav3lli.backup.R
+import com.machiav3lli.backup.ui.compose.item.ActionButton
+import com.machiav3lli.backup.ui.compose.item.ElevatedActionButton
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerDialogUI(
+    state: TimePickerState,
+    openDialogCustom: MutableState<Boolean>,
+    onSave: (hour: Int, minute: Int) -> Unit,
+) {
+    val context = LocalContext.current
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.schedule),
+                style = MaterialTheme.typography.titleLarge
+            )
+            TimePicker(state)
+            Row(
+                Modifier.fillMaxWidth()
+            ) {
+                ActionButton(text = stringResource(id = R.string.dialogCancel)) {
+                    openDialogCustom.value = false
+                }
+                Spacer(Modifier.weight(1f))
+                ElevatedActionButton(text = stringResource(id = R.string.dialogSave)) {
+                    onSave(state.hour, state.minute)
+                    openDialogCustom.value = false
+                }
+            }
+        }
+    }
+}
