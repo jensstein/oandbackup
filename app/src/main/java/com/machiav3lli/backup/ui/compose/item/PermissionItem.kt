5b6ba8b20cb45ee82c7e51b08153cd6d7dd880ad
machiav3lli <antonios.hazim@tutanota.com>
2023-06-02 03:44:28 +0200

    Update: Revamp PermissionItem using ItemList

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/PermissionItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/PermissionItem.kt
@@ -1,19 +1,20 @@
 package com.machiav3lli.backup.ui.compose.item
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -21,52 +22,54 @@ import androidx.compose.ui.unit.dp
 import com.machiav3lli.backup.ui.compose.theme.ColorUpdated
 import com.machiav3lli.backup.ui.item.Permission
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PermissionItem(
     item: Permission,
     onClick: () -> Unit = {},
 ) {
-    OutlinedCard(
+    ListItem(
         modifier = Modifier
-            .fillMaxWidth(),
-        shape = MaterialTheme.shapes.medium,
-        border = BorderStroke(1.dp, MaterialTheme.colorScheme.surface),
-        colors = CardDefaults.outlinedCardColors(
-            containerColor = MaterialTheme.colorScheme.background,
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.large)
+            .clickable(onClick = onClick),
+        colors = ListItemDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
         ),
-        elevation = CardDefaults.cardElevation(4.dp),
-        onClick = onClick
-    ) {
-        Row(
-            modifier = Modifier.wrapContentHeight(),
-        ) {
-            Icon(
-                imageVector = item.icon,
-                contentDescription = stringResource(id = item.nameId),
-                modifier = Modifier.padding(8.dp)
-            )
-            Text(
-                text = stringResource(id = item.nameId),
-                modifier = Modifier.align(Alignment.CenterVertically),
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1,
-                style = MaterialTheme.typography.titleLarge,
-            )
+        headlineContent = {
+            Row(
+                modifier = Modifier.wrapContentHeight(),
+            ) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = stringResource(id = item.nameId),
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text(
+                    text = stringResource(id = item.nameId),
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+        },
+        supportingContent = {
+            Column {
+                Text(
+                    text = stringResource(id = item.descriptionId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                )
+                if (item.warningTextId != -1) {
+                    Text(
+                        text = stringResource(id = item.warningTextId),
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = ColorUpdated,
+                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                    )
+                }
+            }
         }
-        Text(
-            text = stringResource(id = item.descriptionId),
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-        )
-        if (item.warningTextId != -1) {
-            Text(
-                text = stringResource(id = item.warningTextId),
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.Bold,
-                color = ColorUpdated,
-                modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-            )
-        }
-    }
+    )
 }
