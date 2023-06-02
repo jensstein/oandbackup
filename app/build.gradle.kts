2c745c5a0bd1130e42c5e487ca1b697dfe15f5fe
machiav3lli <antonios.hazim@tutanota.com>
2023-06-02 02:06:30 +0200

    Update Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,11 @@ plugins {
 
 val vKotlin = "1.8.21"
 val vComposeCompiler = "1.4.7"
-val vCompose = "1.5.0-alpha04"
+val vCompose = "1.5.0-beta01"
 val vKotlinSerialization = "1.5.1"
 val vRoom = "2.5.1"
-val vNavigation = "2.6.0-rc01"
-val vAccompanist = "0.31.2-alpha"
+val vNavigation = "2.7.0-alpha01"
+val vAccompanist = "0.31.3-beta"
 val vLibsu = "5.0.5"
 //val vIconics = "5.3.4"
 
@@ -130,7 +130,7 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$vKotlinSerialization")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$vKotlinSerialization")
-    implementation("com.charleskorn.kaml:kaml:0.53.0")
+    implementation("com.charleskorn.kaml:kaml:0.54.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
@@ -153,8 +153,8 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:$vCompose")
     implementation("androidx.compose.runtime:runtime-livedata:$vCompose")
     implementation("androidx.navigation:navigation-compose:$vNavigation")
-    implementation("io.coil-kt:coil-compose:2.3.0")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha01")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
     implementation("com.google.accompanist:accompanist-systemuicontroller:$vAccompanist")
     implementation("com.google.accompanist:accompanist-navigation-animation:$vAccompanist")
     implementation("com.google.accompanist:accompanist-permissions:$vAccompanist")
