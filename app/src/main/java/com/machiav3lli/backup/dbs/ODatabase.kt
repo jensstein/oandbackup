f80c08056266a973b823b1e91488597a6f31e092
machiav3lli <antonios.hazim@tutanota.com>
2023-08-24 13:58:50 +0200

    Update: New DB version

--- app/src/main/java/com/machiav3lli/backup/dbs/ODatabase.kt
+++ app/src/main/java/com/machiav3lli/backup/dbs/ODatabase.kt
@@ -47,13 +47,14 @@ import com.machiav3lli.backup.dbs.entity.SpecialInfo
         AppInfo::class,
         SpecialInfo::class,
         Backup::class],
-    version = 7,
+    version = 8,
     exportSchema = true,
     autoMigrations = [
         AutoMigration(from = 2, to = 4),
         AutoMigration(from = 4, to = 5),
         AutoMigration(from = 5, to = 6, spec = ODatabase.Companion.AutoMigration5to6::class),
         AutoMigration(from = 6, to = 7, spec = ODatabase.Companion.AutoMigration6to7::class),
+        AutoMigration(from = 7, to = 8),
     ]
 )
 @TypeConverters(Converters::class)
