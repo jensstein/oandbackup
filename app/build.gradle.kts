8ba2e11eed5f301ff7adac5f5797bf3643d0c805
machiav3lli <antonios.hazim@tutanota.com>
2023-08-24 13:58:30 +0200

    Update: Generate Kotlin code using Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ android {
                 ksp {
                     arg("room.schemaLocation", "$projectDir/schemas")
                     arg("room.incremental", "true")
+                    arg("room.generateKotlin", "true")
                 }
             }
         }
