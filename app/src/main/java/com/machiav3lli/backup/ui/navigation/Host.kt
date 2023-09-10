3d231bb38a21624992e824c10768b62851a37d16
machiav3lli <antonios.hazim@tutanota.com>
2023-09-11 01:32:52 +0200

    Update: Replace Main navigation composable with MainPage

--- app/src/main/java/com/machiav3lli/backup/ui/navigation/Host.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/navigation/Host.kt
@@ -3,7 +3,9 @@ package com.machiav3lli.backup.ui.navigation
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.pager.PagerState
@@ -17,6 +19,7 @@ import androidx.navigation.activity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.machiav3lli.backup.activities.PrefsActivityX
+import com.machiav3lli.backup.pages.MainPage
 import com.machiav3lli.backup.pages.PermissionsPage
 import com.machiav3lli.backup.pages.WelcomePage
 import com.machiav3lli.backup.preferences.ExportsPage
@@ -26,13 +29,10 @@ import com.machiav3lli.backup.preferences.persist_beenWelcomed
 import com.machiav3lli.backup.viewmodels.ExportsViewModel
 import com.machiav3lli.backup.viewmodels.LogViewModel
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController,
-    pagerState: PagerState,
-    pages: List<NavItem>,
 ) {
     NavHost(
         modifier = modifier,
@@ -47,9 +47,7 @@ fun MainNavHost(
             PermissionsPage()
         }
         slideUpComposable(NavItem.Main.destination) {
-            SlidePager(
-                pagerState = pagerState,
-                pageItems = pages,
+            MainPage(
                 navController = navController
             )
         }
