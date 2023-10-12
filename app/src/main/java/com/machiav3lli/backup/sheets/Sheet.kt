6fc1e8056ca4a1bce17ec6bbec5d2df1b4ea873f
machiav3lli <antonios.hazim@tutanota.com>
2023-10-13 01:31:05 +0200

    Update: Remove Sheets drag handle

--- app/src/main/java/com/machiav3lli/backup/sheets/Sheet.kt
+++ app/src/main/java/com/machiav3lli/backup/sheets/Sheet.kt
@@ -1,20 +1,14 @@
 package com.machiav3lli.backup.sheets
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.machiav3lli.backup.preferences.pref_fixNavBarOverlap
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Sheet(
     onDismissRequest: () -> Unit,
@@ -28,33 +22,14 @@ fun Sheet(
     //dragHandle: @Composable() (() -> Unit)?,
     //windowInsets: WindowInsets,
     content: @Composable() (ColumnScope.() -> Unit),
-): Unit {
+) {
     ModalBottomSheet(
         sheetState = sheetState,
         containerColor = MaterialTheme.colorScheme.background,
         scrimColor = Color.Transparent,
+        dragHandle = null,
         onDismissRequest = onDismissRequest
     ) {
-        if (pref_fixNavBarOverlap.value > 0) {
-            Column(
-                modifier = Modifier
-                    .padding(bottom = pref_fixNavBarOverlap.value.dp)
-            ) {
-                content()
-            }
-            // another possibility
-            //Column {
-            //    Column(modifier = Modifier.weight(1f)) {
-            //        content()
-            //    }
-            //    Spacer(modifier = Modifier
-            //        .fillMaxWidth()
-            //        .height(navBarHeight)
-            //        .background(Color.Red),
-            //    )
-            //}
-        } else {
-            content()
-        }
+        content()
     }
 }
