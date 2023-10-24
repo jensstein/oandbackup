bca36ec39bec62ae6e97cdbd42bb842c30a44275
machiav3lli <antonios.hazim@tutanota.com>
2023-10-25 00:15:21 +0200

    Add: Previous Prefs pages to Main NavHost

--- app/src/main/java/com/machiav3lli/backup/ui/navigation/Host.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/navigation/Host.kt
@@ -7,27 +7,22 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.AndroidViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.activity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.machiav3lli.backup.activities.PrefsActivityX
+import com.machiav3lli.backup.OABX
 import com.machiav3lli.backup.pages.MainPage
 import com.machiav3lli.backup.pages.PermissionsPage
+import com.machiav3lli.backup.pages.PrefsPage
 import com.machiav3lli.backup.pages.WelcomePage
 import com.machiav3lli.backup.preferences.ExportsPage
 import com.machiav3lli.backup.preferences.LogsPage
 import com.machiav3lli.backup.preferences.TerminalPage
 import com.machiav3lli.backup.preferences.persist_beenWelcomed
-import com.machiav3lli.backup.viewmodels.ExportsViewModel
-import com.machiav3lli.backup.viewmodels.LogViewModel
 
 @Composable
 fun MainNavHost(
@@ -51,40 +46,20 @@ fun MainNavHost(
                 navController = navController
             )
         }
-        activity(NavItem.Settings.destination) {
-            this.activityClass = PrefsActivityX::class
-        }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun PrefsNavHost(
-    modifier: Modifier = Modifier,
-    navController: NavHostController,
-    pagerState: PagerState,
-    pages: List<NavItem>,
-    viewModels: List<AndroidViewModel>,
-) {
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = NavItem.Settings.destination,
-    ) {
         slideInComposable(NavItem.Settings.destination) {
-            SlidePager(
-                pageItems = pages,
-                pagerState = pagerState,
-                navController = navController,
+            PrefsPage(
+                navController = navController
             )
         }
         slideInComposable(NavItem.Exports.destination) {
-            val viewModel = viewModels.find { it is ExportsViewModel } as ExportsViewModel
-            ExportsPage(viewModel)
+            OABX.main?.exportsViewModel?.let { viewModel ->
+                ExportsPage(viewModel)
+            }
         }
         slideInComposable(NavItem.Logs.destination) {
-            val viewModel = viewModels.find { it is LogViewModel } as LogViewModel
-            LogsPage(viewModel)
+            OABX.main?.logsViewModel?.let { viewModel ->
+                LogsPage(viewModel)
+            }
         }
         slideInComposable(NavItem.Terminal.destination) {
             TerminalPage()
