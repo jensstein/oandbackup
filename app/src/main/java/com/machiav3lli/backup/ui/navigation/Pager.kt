40d7e333367eeef93224871e53d0617480bffcb7
machiav3lli <antonios.hazim@tutanota.com>
2023-11-09 02:41:34 +0100

    Fix: Navigation bar item's ripple

--- app/src/main/java/com/machiav3lli/backup/ui/navigation/Pager.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/navigation/Pager.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -166,6 +167,7 @@ fun RowScope.NavBarItem(
     Row(
         modifier = modifier
             .padding(vertical = 8.dp)
+            .clip(MaterialTheme.shapes.extraLarge)
             .clickable { onClick() }
             .background(
                 background,
