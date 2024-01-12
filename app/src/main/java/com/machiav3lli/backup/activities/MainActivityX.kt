jensstein/oandbackup

commit 38dce2ded95d7ac7f27f4b933786fa929b51c10b

add --bypass-low-target-sdk-block to pm install, ensure same option order, make have* lazy (checked only once), fix naming apkPath -> apkFile

app/src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
@@ -31,12 +31,14 @@ import androidx.appcompat.app.AlertDialog
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.navigation.NavHostController
@@ -234,12 +236,12 @@ class MainActivityX : BaseActivity() {
                 Scaffold(
                     containerColor = MaterialTheme.colorScheme.background,
                     contentColor = MaterialTheme.colorScheme.onBackground,
-                ) {
+                ) { paddingValues ->
                     ObservedEffect {
                         resumeMain()
                     }
 
-                    Box {
+                    Box(modifier = Modifier.padding(paddingValues)) {
                         MainNavHost(
                             navController = navController,
                         )

app/src/main/java/com/machiav3lli/backup/actions/RestoreAppAction.kt
@@ -30,6 +30,7 @@ import com.machiav3lli.backup.dbs.entity.Backup
 import com.machiav3lli.backup.handler.LogsHandler
 import com.machiav3lli.backup.handler.ShellHandler
 import com.machiav3lli.backup.handler.ShellHandler.Companion.findAssetFile
+import com.machiav3lli.backup.handler.ShellHandler.Companion.hasPmBypassLowTargetSDKBlock
 import com.machiav3lli.backup.handler.ShellHandler.Companion.quote
 import com.machiav3lli.backup.handler.ShellHandler.Companion.quoteMultiple
 import com.machiav3lli.backup.handler.ShellHandler.Companion.runAsRoot
@@ -1011,21 +1012,22 @@ open class RestoreAppAction(context: Context, work: AppActionWork?, shell: Shell
     }
 
     private fun getPackageInstallCommand(
-        apkPath: RootFile,
+        apkFile: RootFile,
         profileId: Int,
         basePackageName: String? = null,
     ): String =
         listOfNotNull(
-            "cat", quote(apkPath.absolutePath),
+            "cat", quote(apkFile.absolutePath),
             "|",
             "pm", "install",
             basePackageName?.let { "-p $basePackageName" },
             if (isRestoreAllPermissions) "-g" else null,
             if (isAllowDowngrade) "-d" else null,
+            if (hasPmBypassLowTargetSDKBlock) "--bypass-low-target-sdk-block" else null,
             "-i ${pref_installationPackage.value}",
             "-t",
             "-r",
-            "-S", apkPath.length().toString(),
+            "-S", apkFile.length(),
             "--user", profileId,
         ).joinToString(" ")
 
@@ -1036,26 +1038,27 @@ open class RestoreAppAction(context: Context, work: AppActionWork?, shell: Shell
     ): String =
         listOfNotNull(
             "pm", "install-create",
-            "-i", pref_installationPackage.value,
-            "--user", profileId,
-            "-r",
-            "-t",
             if (isRestoreAllPermissions) "-g" else null,
             if (isAllowDowngrade) "-d" else null,
-            "-S", sumSize
+            if (hasPmBypassLowTargetSDKBlock) "--bypass-low-target-sdk-block" else null,
+            "-i", pref_installationPackage.value,
+            "-t",
+            "-r",
+            "-S", sumSize,
+            "--user", profileId,
         ).joinToString(" ")
 
     private fun getSessionWriteCommand(
-        apkPath: RootFile,
+        apkFile: RootFile,
         sessionId: Int,
     ): String =
         listOfNotNull(
-            "cat", quote(apkPath.absolutePath),
+            "cat", quote(apkFile.absolutePath),
             "|",
             "pm", "install-write",
-            "-S", apkPath.length(),
+            "-S", apkFile.length(),
             sessionId,
-            apkPath.name,
+            apkFile.name,
         ).joinToString(" ")
 
 

app/src/main/java/com/machiav3lli/backup/handler/ShellHandler.kt
@@ -19,12 +19,14 @@ package com.machiav3lli.backup.handler
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.os.Environment.DIRECTORY_DOCUMENTS
 import androidx.core.text.isDigitsOnly
 import com.machiav3lli.backup.OABX
 import com.machiav3lli.backup.OABX.Companion.addErrorCommand
 import com.machiav3lli.backup.OABX.Companion.isDebug
 import com.machiav3lli.backup.handler.ShellHandler.FileInfo.Companion.utilBoxInfo
+import com.machiav3lli.backup.preferences.baseInfo
 import com.machiav3lli.backup.traceDebug
 import com.machiav3lli.backup.utils.BUFFER_SIZE
 import com.machiav3lli.backup.utils.FileUtils.translatePosixPermissionToMode
@@ -164,7 +166,7 @@ class ShellHandler {
         Shell.setDefaultBuilder(shellDefaultBuilder())
         needFreshShell(startup = true)
 
-        suInfo().forEach { Timber.i(it) }
+        baseInfo().forEach { Timber.i(it) }
 
         utilBoxes = mutableListOf<UtilBox>()
         try {
@@ -644,25 +646,41 @@ class ShellHandler {
         val hasRootShell get() = Shell.getShell().status >= Shell.ROOT_SHELL
         val hasMountMaster get() = Shell.getShell().status >= Shell.ROOT_MOUNT_MASTER
 
-        val hasMountMasterOption: Boolean
-            get() {
+        val hasMountMasterOption: Boolean by lazy {
                 // only return true if command does not choke on the option and exits with code 0
-                val ok = runCatching {
+                runCatching {
                     ShellUtils.fastCmdResult("echo true | su --mount-master")
                 }.getOrNull() ?: false
-                //traceDebug { "detected mount master option = $ok" }
-                return ok
             }
 
-        val hasNsEnter: Boolean
-            get() {
+        val hasNsEnter: Boolean by lazy {
                 // only return true if command does not choke on the option and exits with code 0
-                val ok = runCatching {
+            runCatching {
                     ShellUtils.fastCmdResult("nsenter --help")
                 }.getOrNull() ?: false
-                //traceDebug { "detected nsenter can be invoked = $ok" }
-                return ok
             }
+
+        val hasPmBypassLowTargetSDKBlock: Boolean by lazy {
+            runCatching {
+                // the option is not include in usage output, instead check,
+                // if the error message is complaining about the missing file
+                // and not about that option
+                ShellUtils.fastCmdResult(
+                    "pm install --bypass-low-target-sdk-block not-existing.apk 2>&1 | grep not-existing.apk"
+                )
+                        &&
+                        ! ShellUtils.fastCmdResult(
+                            "pm install --bypass-low-target-sdk-block not-existing.apk 2>&1 | grep bypass-low-target-sdk-block"
+                        )
+            }.getOrNull() ?: false
+        }
+
+        fun sysInfo() =
+            listOf(
+                "system       = Android ${Build.VERSION.RELEASE} - ${ShellUtils.fastCmd("uname -r -m -o")}",
+                "has nsenter  = $hasNsEnter",
+                "has bypass-low-target-sdk-block = $hasPmBypassLowTargetSDKBlock",
+            )
 
         fun suInfo() =
             listOf(
@@ -679,7 +697,6 @@ class ShellHandler {
                 "libsu has root shell       = $hasRootShell",
                 "libsu has mount master     = $hasMountMaster",
                 "su has mount master option = $hasMountMasterOption",
-                "system has nsenter         = $hasNsEnter",
             )
 
         class ShellInit : Shell.Initializer() {

app/src/main/java/com/machiav3lli/backup/preferences/TerminalPage.kt
@@ -87,6 +87,7 @@ import com.machiav3lli.backup.handler.ShellHandler.Companion.runAsRoot
 import com.machiav3lli.backup.handler.ShellHandler.Companion.runAsRootPipeOutCollectErr
 import com.machiav3lli.backup.handler.ShellHandler.Companion.suCommand
 import com.machiav3lli.backup.handler.ShellHandler.Companion.suInfo
+import com.machiav3lli.backup.handler.ShellHandler.Companion.sysInfo
 import com.machiav3lli.backup.handler.ShellHandler.Companion.utilBox
 import com.machiav3lli.backup.handler.ShellHandler.FileInfo.Companion.utilBoxInfo
 import com.machiav3lli.backup.handler.findBackups
@@ -152,14 +153,16 @@ fun shell(command: String, silent: Boolean = false): List<String> {
 fun appInfo(): List<String> {
     return listOf(
         "------ application",
-        BuildConfig.APPLICATION_ID,
-        BuildConfig.VERSION_NAME,
-        applicationIssuer?.let { "signed by $it" } ?: "",
+        "package   = ${BuildConfig.APPLICATION_ID}",
+        "version   = ${BuildConfig.VERSION_NAME}",
+        applicationIssuer?.let { "signed by = $it" } ?: "unsigned",
     )
 }
 
 fun baseInfo(): List<String> {
     return appInfo() +
+            listOf("------ system") +
+            sysInfo() +
             listOf("------ superuser") +
             suInfo() +
             listOf("------ shell utility box") + utilBoxInfo()

commit 9895a2a9216647128edd6367084aab194c7e64b8

Fix: Abnormal padding (caused by 38dce2de)

app/src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
@@ -17,6 +17,7 @@
  */
 package com.machiav3lli.backup.activities
 
+import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.DialogInterface
 import android.content.Intent
@@ -31,14 +32,12 @@ import androidx.appcompat.app.AlertDialog
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.navigation.NavHostController
@@ -121,6 +120,7 @@ class MainActivityX : BaseActivity() {
         LogViewModel.Factory(application)
     }
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         val mainChanged = (this != OABX.mainSaved)
@@ -236,12 +236,12 @@ class MainActivityX : BaseActivity() {
                 Scaffold(
                     containerColor = MaterialTheme.colorScheme.background,
                     contentColor = MaterialTheme.colorScheme.onBackground,
-                ) { paddingValues ->
+                ) {
                     ObservedEffect {
                         resumeMain()
                     }
 
-                    Box(modifier = Modifier.padding(paddingValues)) {
+                    Box {
                         MainNavHost(
                             navController = navController,
                         )
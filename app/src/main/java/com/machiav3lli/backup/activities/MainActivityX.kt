9895a2a9216647128edd6367084aab194c7e64b8
machiav3lli <antonios.hazim@tutanota.com>
2024-01-12 01:14:40 +0100

    Fix: Abnormal padding (caused by 38dce2de)

--- app/src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
+++ app/src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
@@ -17,6 +17,7 @@
  */
 package com.machiav3lli.backup.activities
 
+import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.DialogInterface
 import android.content.Intent
@@ -31,14 +32,12 @@ import androidx.appcompat.app.AlertDialog
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.navigation.NavHostController
@@ -121,6 +120,7 @@ class MainActivityX : BaseActivity() {
         LogViewModel.Factory(application)
     }
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         val mainChanged = (this != OABX.mainSaved)
@@ -236,12 +236,12 @@ class MainActivityX : BaseActivity() {
                 Scaffold(
                     containerColor = MaterialTheme.colorScheme.background,
                     contentColor = MaterialTheme.colorScheme.onBackground,
-                ) { paddingValues ->
+                ) {
                     ObservedEffect {
                         resumeMain()
                     }
 
-                    Box(modifier = Modifier.padding(paddingValues)) {
+                    Box {
                         MainNavHost(
                             navController = navController,
                         )
