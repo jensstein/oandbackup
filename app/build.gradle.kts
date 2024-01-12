7d8c6cea54c5162b9373bfa0ac92fc3afe369262
machiav3lli <antonios.hazim@tutanota.com>
2024-01-12 01:16:12 +0100

    Update Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,34 +20,34 @@ import com.android.build.gradle.internal.tasks.factory.dependsOn
 plugins {
     id("com.android.application")
     kotlin("android")
-    kotlin("plugin.serialization").version("1.9.20")
-    id("com.google.devtools.ksp") version ("1.9.20-1.0.14")
+    kotlin("plugin.serialization").version("1.9.22")
+    id("com.google.devtools.ksp") version ("1.9.22-1.0.16")
 }
 
-val vActivity = "1.8.1"
+val vActivity = "1.8.2"
 val vAccompanist = "0.33.2-alpha"
 val vBiometric = "1.2.0-alpha05"
 val vCoil = "2.5.0"
 val vCommonsCompress = "1.25.0"
-val vCommonsIO = "2.14.0"
-val vComposeCompiler = "1.5.4"
-val vCompose = "1.6.0-alpha07"
+val vCommonsIO = "2.15.1"
+val vComposeCompiler = "1.5.8"
+val vCompose = "1.6.0-beta03"
 val vComposeM3 = "1.1.2" // does NOT crash in context menu "Put"
 val vDatastore = "1.0.0"
-val vKAML = "0.55.0"
-val vKotlin = "1.9.20"
-val vKSP = "1.0.14"
-val vLibsu = "5.2.1"
+val vKAML = "0.56.0"
+val vKotlin = "1.9.22"
+val vKSP = "1.0.16"
+val vLibsu = "5.2.2"
 val vLifecycle = "2.6.2"
-val vMaterial = "1.10.0"
-val vNavigation = "2.7.5"
+val vMaterial = "1.11.0"
+val vNavigation = "2.7.6"
 val vPreference = "1.2.1"
-val vRoom = "2.6.0"
+val vRoom = "2.6.1"
 val vSecurity = "1.1.0-alpha06"
-val vSemVer = "4.1.0"
-val vSerialization = "1.6.1"
+val vSemVer = "4.2.1"
+val vSerialization = "1.6.2"
 val vTimber = "5.0.1"
-val vWork = "2.9.0-rc01"
+val vWork = "2.9.0"
 
 val vTest = "1.5.0"
 val vTestRules = "1.5.0"
